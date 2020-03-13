e7b9c475dfa5eeb138f86f2ef881f9277b2d39b4
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-03-13 23:33:11 +0300

    layout: ListView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,13 @@ class MyApp extends StatelessWidget {
   final String title = 'Hello Flutter';
 
   _buildScrollableContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: List.generate(26, (index) {
-        int c = 255 - index * 10;
-        return Container(color: Color.fromARGB(255, c, c, c), height: 50);
-      }),
-    );
+    return ListView.builder(
+        itemCount: 255,
+        itemBuilder: (context, index) {
+          print('lazy build $index');
+          int c = 255 - index * 10;
+          return Container(color: Color.fromARGB(255, c, c, c), height: 50);
+        });
   }
 
   @override
@@ -54,9 +54,7 @@ class MyApp extends StatelessWidget {
                   child: Container(
                       color: Colors.white,
                       constraints: BoxConstraints.expand(),
-                      child: Scrollbar(
-                          child: SingleChildScrollView(
-                              child: _buildScrollableContent())))),
+                      child: Scrollbar(child: _buildScrollableContent()))),
               Expanded(
                   flex: 1,
                   child: Stack(alignment: Alignment.bottomRight, children: [
